b1d6da99955546e4c96ccf0aeec91ff3af0aa5b0
jvalero4 <javiervalerogamez@gmail.com>
2024-05-03 11:43:00 +0200

    added all test exercises of part 4a

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,77 @@ const dummy = (blogs) => {
         return 0
     }
   }
+
+const totalLikes = (blogs) => {
+    const blogsLikes = blogs.map(blog => blog.likes);
+
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return blogsLikes.length === 0 ? 0 : blogsLikes.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const blogsLikes = blogs.map(blog => blog.likes);
+    
+    const indexOfMostLiked = blogsLikes.indexOf(Math.max(...blogsLikes))
+
+    const blogFavorite = {
+        title: blogs[indexOfMostLiked].title,
+        author: blogs[indexOfMostLiked].author,
+        likes: blogs[indexOfMostLiked].likes
+    }
+    return blogFavorite
+    
+}
+
+const mostBlogs = (blogs) => {
+    const authorBlogsCount = new Map(blogs.map(
+        blog => [blog.author, blogs.filter(b => b.author === blog.author).length]));
+
+    let maxAuthor = "";
+    let maxBlogs = 0;
+
+    authorBlogsCount.forEach((blogs, author) => {
+        if (blogs > maxBlogs) {
+            maxAuthor = author
+            maxBlogs = blogs
+        }
+    })
+
+    return {
+        author: maxAuthor,
+        blogs: maxBlogs
+    }
+}
+
+const mostLikes = (blogs) => {
+    const authorLikesCount = blogs.reduce((acc, blog) => {
+        if (acc[blog.author]) {
+          acc[blog.author] += blog.likes;
+        } else {
+          acc[blog.author] = blog.likes;
+        }
+        return acc;
+      }, {});
+
+    let maxAuthor = ''
+    let maxLikes = 0
+
+    for (const author in authorLikesCount) {
+        if(authorLikesCount[author] > maxLikes) {
+            maxAuthor = author
+            maxLikes = authorLikesCount[author]
+        }
+    }
+        
+    
+    return {
+        author: maxAuthor,
+        likes: maxLikes
+    }
+}
   
   module.exports = {
-    dummy
+    dummy,totalLikes,favoriteBlog,mostBlogs,mostLikes
   }
